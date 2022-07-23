ba50eb1955cb8f16acd572f2b7618232c6fd1a87
tintinthong <justinthong93@gmail.com>
2022-07-23 11:21:41 +0100

    update init to be explicit

--- did_resolver/__init__.py
+++ did_resolver/__init__.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-from .resolver import Resolver, parse
+from .resolver import (
+    DIDDocument,
+    DIDResolutionResult,
+    ParsedDID,
+    Resolvable,
+    Resolver,
+    parse,
+)
+
+__all__ = [
+    "DIDDocument",
+    "DIDResolutionResult",
+    "ParsedDID",
+    "Resolvable",
+    "Resolver",
+    "parse",
+]
